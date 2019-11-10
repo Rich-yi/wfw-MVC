6af49b5212b2209af05e8569e4d770bb61e22e9c
MrWnag <934105927@qq.com>
2019-11-09 22:59:55 -0800

    初始化路由

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package wfw_MVC
+package main
+
+import (
+
+	"github.com/gin-gonic/gin"
+
+)
+
+func main() {
+	//初始化路由
+	routher:=gin.New()
+
+	//请求分配
+	r1:=routher.Group("V1")
+{
+	r1.GET("/cat", func(context *gin.Context) {
+		context.Writer.WriteString("bigcat")
+	})
+
+	routher.Run(":8001")
+}
+
+	}
