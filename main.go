43f359486a5a7eccb4fbbd9dc1fa0eb2bbe01159
MrWnag <934105927@qq.com>
2019-11-10 18:46:58 -0800

    初始化项目

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"wfw-MVC/model"
+	"fmt"
 )
 
 func main() {
 	//初始化路由
 	routher := gin.New()
-
+	err:=model.InitDb()
+	if err != nil {
+		fmt.Println("数据库创建失败",err)
+		return
+	}
 	//请求分配
 	r1 := routher.Group("V1")
 	{
@@ -17,10 +22,6 @@ func main() {
 			context.Writer.WriteString("bigcat")
 		})
 
-
 	}
-	model.InitModel()
-	model.InitData()
-	model.SearchData()
 	routher.Run(":8001")
 }
